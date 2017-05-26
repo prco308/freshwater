251e6efd7dc7a0f4b0529bea4d1f42d497729a39
alvis <alvisb94@gmail.com>
2017-05-26 16:09:59 +0100

    server port

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 var express = require('express');
 var http  = require('http');
 var app = express();
-app.set('port', process.env.PORT || 3000);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.set('port', server_port);
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
@@ -169,6 +171,6 @@ function submitQuestion(data){
 
 
 // start server
-server.listen(3000, function () {
-    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
+server.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
