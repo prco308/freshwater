966bacaf66cb84b08e768f25a0946d4ff0259e9b
alvis <alvisb94@gmail.com>
2017-05-26 19:13:53 +0100

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var http  = require('http');
 var app = express();
-var port = process.env.PORT;
+var port = process.env.PORT || 3000;
 app.set('port', port);
 var session = require('express-session');
 var bodyParser = require('body-parser');
